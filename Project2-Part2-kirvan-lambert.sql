1c5fd0f08a70ee8b2b6c4196419b39355ac62244
Ben Lambert <>
2020-11-05 17:13:18 -0600

    megatable done

--- Project2-Part2-kirvan-lambert.sql
+++ Project2-Part2-kirvan-lambert.sql
@@ -6,7 +6,7 @@
 DROP DATABASE IF EXISTS temp_db;
 CREATE DATABASE temp_db; 
 
-#create megatable 
+-- TABLE with real datatypes
 DROP TABLE IF EXISTS temp_data; 
 CREATE TABLE temp_data
 (city_dt DATE, 
@@ -42,12 +42,61 @@ LandMinTemperatureUncertainty DECIMAL(10,4) UNSIGNED,
 LandAndOceanAverageTemperature DECIMAL(10, 6), 
 LandAndOceanAverageTemperatureUncertainty DECIMAL(10,4) UNSIGNED
 ); 
+
+
+
+-- TABLE with all VARCHAR values for easy insertion
+
+DROP TABLE IF EXISTS temp_data; 
+CREATE TABLE temp_data
+(city_dt DATE, 
+city_AverageTemperature VARCHAR(100), #DECIMAL(10, 6), 
+city_AverageTemperatureUncertainty VARCHAR(100), #DECIMAL(10,4) UNSIGNED, 
+city_City VARCHAR(100), #VARCHAR(40), 
+city_Country VARCHAR(100), #VARCHAR (40), 
+city_Latitude VARCHAR(100), #VARCHAR (7), 
+city_Longitude VARCHAR(100), #VARCHAR (7), 
+country_dt VARCHAR(100), #DATE, 
+country_AverageTemperature VARCHAR(100), #DECIMAL(10, 6), 
+country_AverageTemperatureUncertainty VARCHAR(100), #DECIMAL(10,4) UNSIGNED, 
+country_Country VARCHAR(100), #VARCHAR (40), 
+mCity_dt VARCHAR(100), #DATE, 
+mCity_AverageTemperature VARCHAR(100), #DECIMAL(10, 6), 
+mCity_AverageTemperatureUncertainty VARCHAR(100), #DECIMAL(10,4) UNSIGNED, 
+mCity_City VARCHAR(100), #VARCHAR(40),
+mCity_Country VARCHAR(100), #VARCHAR(40), 
+mCity_Latitude VARCHAR(100), #VARCHAR(7), 
+mCity_Longitude VARCHAR(100), #VARCHAR(40), 
+state_dt VARCHAR(100), #DATE, 
+state_AverageTemperature VARCHAR(100), #DECIMAL(10, 6), 
+state_AverageTemperatureUncertainty VARCHAR(100), #DECIMAL(10,4) UNSIGNED, 
+state_State VARCHAR(100), #VARCHAR(40),
+state_Country VARCHAR(100), #VARCHAR(40), 
+dt VARCHAR(100), #DATE, 
+LandAverageTemperature VARCHAR(100), #DECIMAL(10, 6), 
+LandAverageTemperatureUncertainty VARCHAR(100), #DECIMAL(10,4) UNSIGNED, 
+LandMaxTemperature VARCHAR(100), #DECIMAL(10, 6), 
+LandMaxTemperatureUncertainty VARCHAR(100), #DECIMAL(10,4) UNSIGNED, 
+LandMinTemperature VARCHAR(100), #DECIMAL(10, 6), 
+LandMinTemperatureUncertainty VARCHAR(100), #DECIMAL(10,4) UNSIGNED, 
+LandAndOceanAverageTemperature VARCHAR(100), #DECIMAL(10, 6), 
+LandAndOceanAverageTemperatureUncertainty VARCHAR(100) #DECIMAL(10,4) UNSIGNED
+); 
  
+
+
+
+
+
+VARCHAR(100), #
+
+
+
 
 #load data from files
 
 #file 1
-LOAD DATA LOCAL INFILE 'GlobalTemperatures.csv' INTO TABLE temp_data
+LOAD DATA INFILE '/Applications/MAMP/tmp/archive/GlobalTemperatures.csv' INTO TABLE temp_data
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
@@ -55,9 +104,13 @@ IGNORE 1 LINES
 (dt,LandAverageTemperature, LandAverageTemperatureUncertainty, LandMaxTemperature, 
     LandMaxTemperatureUncertainty, LandMinTemperature, LandMinTemperatureUncertainty, 
     LandAndOceanAverageTemperature, LandAndOceanAverageTemperatureUncertainty); 
+    
+
+
+
 
 #file2
-LOAD DATA LOCAL INFILE 'GlobalLandTemperaturesByCity.csv' INTO TABLE temp_data
+LOAD DATA INFILE '/Applications/MAMP/tmp/archive/GlobalLandTemperaturesByCity.csv' INTO TABLE temp_data
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
@@ -66,8 +119,10 @@ IGNORE 1 LINES
 	city_City,city_Country, city_Latitude, city_Longitude); 
 
 
+
+
 #file3
-LOAD DATA LOCAL INFILE 'GlobalLandTemperaturesByCountry.csv' INTO TABLE temp_data
+LOAD DATA INFILE '/Applications/MAMP/tmp/archive/GlobalLandTemperaturesByCountry.csv' INTO TABLE temp_data
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
@@ -76,7 +131,7 @@ IGNORE 1 LINES
 
 
 #file4
-LOAD DATA LOCAL INFILE 'GlobalLandTemperaturesByMajorCity.csv' INTO TABLE temp_data
+LOAD DATA INFILE '/Applications/MAMP/tmp/archive/GlobalLandTemperaturesByMajorCity.csv' INTO TABLE temp_data
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
@@ -86,7 +141,7 @@ IGNORE 1 LINES
 
 
 #file5
-LOAD DATA LOCAL INFILE 'GlobalLandTemperaturesByState.csv' INTO TABLE temp_data
+LOAD DATA INFILE '/Applications/MAMP/tmp/archive/GlobalLandTemperaturesByState.csv' INTO TABLE temp_data
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
